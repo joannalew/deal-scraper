b89ba8869a36bbbc6bed3ac39f9fda79977c9a46
Dennis <dmyasnyankin@berkeley.edu>
2019-01-02 12:36:32 -0800

    Create user model with schema info

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
