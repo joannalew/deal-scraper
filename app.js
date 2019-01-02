60391565dec6623c80b16b777e1b90e6bf78a54b
Dennis <dmyasnyankin@berkeley.edu>
2019-01-02 11:49:45 -0800

    update package.json scripts, create Express routes, connect db to mLab

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const users = require("./routes/api/users");
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Deal Scraper Flex Project"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
