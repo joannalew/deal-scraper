f739933b4159f79b5273a8b3c7afb02f70941548
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-03 12:45:07 -0800

    Add routr to login form

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
-// import LoginFormContainer from './session/login_form_container';
+import LoginFormContainer from './session/login_form_container';
 // import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
@@ -12,8 +12,8 @@ const App = () => (
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
-            {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
         </Switch>
     </div>
 );
