9bafde5731f6b2a0b495d83de15ba641f4da8131
Dennis <dmyasnyankin@berkeley.edu>
2019-01-03 10:49:06 -0800

    Add log in functionality to user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
+
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -32,6 +36,45 @@ router.post('/register', (req, res) => {
                     })
                 })
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    // const { errors, isValid } = validateLoginInput(req.body);
+
+    // if (!isValid) {
+    //     return res.status(400).json(errors);
+    // }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'This user does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = { id: user.id, username: user.username };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            // Tell the key to expire in one hour
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
         })
 })
 
