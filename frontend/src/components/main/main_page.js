24184587a3e83adde12a28cd42e3c28283c4aceb
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-02 21:15:42 -0800

    Remove twitter text

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,9 +5,9 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>A Twitter Clone</h1>
+                <h1></h1>
                 <footer>
-                    Copyright &copy; 2019 Chirper
+                    
                 </footer>
             </div>
         );
