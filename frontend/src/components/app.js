e2c96735faa37a92dd7536b1aa90d1d027b9f27a
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-02 21:15:27 -0800

    Comment out code

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,16 +4,16 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
+// import LoginFormContainer from './session/login_form_container';
+// import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div>
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
-            <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
         </Switch>
     </div>
 );
