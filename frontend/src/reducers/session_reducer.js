d5eddb342a8dbdda758cfb7d6793f1ab0426b1c4
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-03 10:53:29 -0800

    Add cases for recieve current user and user sign in

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,12 +1,30 @@
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { RECEIVE_USER_LOGOUT, RECEIVE_CURRENT_USER, RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
+import { merge } from 'lodash';
 
-export default function (state = {}, action) {
+const defaultState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+export default function (state = defaultState, action) {
+    Object.freeze(state);
+
     switch (action.type) {
         case RECEIVE_USER_LOGOUT:
-            return {
+            return ({
                 isAuthenticated: false,
                 user: undefined
-            };
+            });
+
+        case RECEIVE_CURRENT_USER:
+            return ({
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            });
+        case RECEIVE_USER_SIGN_IN:
+            let newState = merge({}, state);
+            newState.isSignedIn = true;
+            return newState;
         default:
             return state;
     }
