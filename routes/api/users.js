cb7faf585001f0f969a77e62d647b845c32a0d77
Dennis <dmyasnyankin@berkeley.edu>
2019-01-03 09:37:38 -0800

    Add registration to user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,4 +5,34 @@ const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+router.post('/register', (req, res) => {
+    // Check to make sure nobody has already registered with a duplicate email
+    // Do we need one of these checks for username as well?
+    User.findOne({ email: req.body.email }) 
+    // User.findOne({ email: req.body.email }) || User.findOne({ username: req.body.username })
+        .then(user => {
+            if (user) {
+                // Throw a 400 error if the email address already exists
+                return res.status(400).json({ email: "A user has already registered with this email" })
+            } else {
+                // Otherwise create a new user
+                const newUser = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+})
+
 module.exports = router;
