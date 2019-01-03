929dc70dd3ae62a7d57dd79a34257e754882c42c
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-03 11:08:38 -0800

    Fix import issue

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,5 +1,6 @@
-import { combineReducers } from 'react-redux';
+import { combineReducers } from 'redux';
 import session from './session_errors_reducer';
+
 
 export default combineReducers({
     session
