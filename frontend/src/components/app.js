a5a49b5453cf966e16887e68bb71c176d8f4f2aa
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-04 13:24:31 -0800

    Add route to item show

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+import ItemShowContainer from './item/item_show_container';
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
@@ -14,6 +15,8 @@ const App = () => (
             <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
+            <Route exact path='/item/:id' component={ItemShowContainer} />
+
         </Switch>
     </div>
 );
