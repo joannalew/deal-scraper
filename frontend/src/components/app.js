0dfdc833797c1b31814a95a1f2b2da791041152d
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-04 10:39:28 -0800

    Add routes for signup form

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,7 +5,7 @@ import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
-// import SignupFormContainer from './session/signup_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
     <div>
@@ -13,7 +13,7 @@ const App = () => (
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
-            {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
         </Switch>
     </div>
 );
