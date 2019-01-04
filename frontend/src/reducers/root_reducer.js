a5eef2cdc35b738aafce24a7031f9344a805db8e
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-04 11:57:55 -0800

    Add entities to root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
+import entities from './entities_reducer';
 
 const RootReducer = combineReducers({
+    entities,
     session,
     errors
 });
