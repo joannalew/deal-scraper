c1a1fd41b584b1b8434400c20996dc1fadcce02a
Joanna <lewj@oregonstate.edu>
2019-01-04 11:29:19 -0800

    update dependencies with promise

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,51 +1,46 @@
 const express = require("express");
 const request = require('request');
+const Promise = require('promise');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
 const router = express.Router();
 
 const ebayKey = require('../../config/keys').ebayAPIKey;
 
-router.get('/', (req, res) => res.json({msg: "This is the search route"}));
+router.get('/', (req, res) => res.json({ msg: "This is the search route" }));
 router.get("/test", (req, res) => res.json({ msg: "This is the search test route" }));
 
-const changeImageFromEbay = function(items, images, res) {
-    for (var key in items) {
-        items[key].storeImg = images[parseInt(key) - 1]
-    }
-    res.send(items);
-}
-
-const getImageFromEbay = function(itemObj, res) {
-    let picAry = [];
+const getImageFromEbay = function (itemObj, res) {
+    var promiseArray = [];
 
     for (var key in itemObj) {
         let url = itemObj[key].storeUrl;
-        request(url, function (error, response, html) {
-            if (!error) {
-                var $ = cheerio.load(html);
-                var src = $('#icImg').attr('src');
-                picAry.push(src);
-                changeImageFromEbay(itemObj, picAry, res);
-            }
-        });
+        promiseArray.push(new Promise((resolve, reject) =>
+            request(url, function (error, response, html) {
+                if (error) { reject(error); }
+                resolve(html);
+            })
+        ))
     }
+
+    res.send(Promise.all(promiseArray));
 };
 
-router.get('/ebay', function(req, res) {
+router.get('/ebay/:keywords', function (req, res) {
     let callback = '_cb_findItemsByKeywords';
-    let keywords = 'nike%20air%20jordan';
+    let keywords = req.params.keywords;
 
     var url = "http://svcs.ebay.com/services/search/FindingService/v1";
     url += "?OPERATION-NAME=findItemsByKeywords";
     url += "&SERVICE-VERSION=1.0.0";
-    url += `&SECURITY-APPNAME=${ ebayKey }`;
+    url += `&SECURITY-APPNAME=${ebayKey}`;
     url += "&GLOBAL-ID=EBAY-US";
     url += "&RESPONSE-DATA-FORMAT=JSON";
-    url += `&callback=${ callback }`;
+    url += `&callback=${callback}`;
     url += "&REST-PAYLOAD";
-    url += `&keywords=${ keywords }`;
+    url += `&keywords=${keywords}`;
 
-    request(url, function(error, response) {
+    request(url, function (error, response) {
         if (!error) {
             let resStr = JSON.stringify(response);
             let resObj = JSON.parse(resStr);
@@ -55,22 +50,21 @@ router.get('/ebay', function(req, res) {
 
             let count = resReal.searchResult[0]['@count'];
             let items = resReal.searchResult[0].item;
-
             let itemInfo = {};
             let idArray = [];
-            
+
             for (let i = 0; i < count; i++) {
                 if (idArray.includes(items[i].itemId[0])) { continue; }
                 idArray.push(items[i].itemId[0]);
 
-                itemInfo[idArray.length] = 
+                itemInfo[idArray.length] =
                     {
-                      store: 'ebay',
-                      storeId: items[i].itemId[0], 
-                      storeUrl: items[i].viewItemURL[0],
-                      storeImg: items[i].galleryURL[0],
-                      title: items[i].title[0],
-                      price: items[i].sellingStatus[0].convertedCurrentPrice[0].__value__ + ' ' + items[i].sellingStatus[0].currentPrice[0]['@currencyId']
+                        store: 'ebay',
+                        storeId: items[i].itemId[0],
+                        storeUrl: items[i].viewItemURL[0],
+                        storeImg: items[i].galleryURL[0],
+                        title: items[i].title[0],
+                        price: items[i].sellingStatus[0].convertedCurrentPrice[0].__value__ + ' ' + items[i].sellingStatus[0].currentPrice[0]['@currencyId']
                     };
             }
 
