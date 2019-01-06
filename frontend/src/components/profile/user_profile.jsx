ef083917d04d8c123ce77222da38eb13c8c95e8c
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-06 14:26:10 -0800

    Add info into profile page

--- frontend/src/components/profile/user_profile.jsx
+++ frontend/src/components/profile/user_profile.jsx
@@ -5,8 +5,20 @@ class UserProfile extends React.Component {
     render() {
 
         return (
-            <div>
-                This is the user profile page
+            <div className='user-profile'>
+                <div className='user-profile-info'>
+                    <div className='user-profile-img'>
+                        <img src="https://profiles.utdallas.edu/img/default.png" alt=""/>
+                    </div>
+                    <div className='user-profile-details'>
+                        <div className='user-profile-username'>{this.props.user.username}</div>
+                        <div className='user-profile-email'>{this.props.user.email}</div>
+                        <div className='user-profile-watch-count'>Number of items watched:</div>
+                    </div>
+                </div>
+                <div className='user-profile-watchlist'>
+
+                </div>
             </div>
         )
     }
