b40214a543b5a94ae5f4f36df6d0e7bc0a0f362a
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-06 14:01:31 -0800

    Create skeleton for user profile component

---
+++ frontend/src/components/profile/user_profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                This is the user profile page
+            </div>
+        )
+    }
+}
