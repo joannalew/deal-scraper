41483cc650cffa087a49b430c326fca6086cb780
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-05 21:52:24 -0800

    Add info to item show page

--- frontend/src/components/item/item_show.jsx
+++ frontend/src/components/item/item_show.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 class ItemShow extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
         let id = this.props.match.params.id;
         this.props.getItem(id);
     }
 
     render() {
-        return (
-            <div className='item-show-page'>
-                <h1>This is the item show page</h1>
-            </div>
-        )
+        if (this.props.item) {
+        
+            return (
+                <div className='item-show-page'>
+                    <h1 className='item-show-title'>{this.props.item.title}</h1>
+                    <img src={this.props.item.storeImg} alt=""/>
+                    <a className='store-link' href={this.props.item.storeUrl}>{this.props.item.store}</a>
+                    <div className='item-price'>{this.props.item.price}</div>
+                </div>
+            )
+        } else {
+            return <div>No Item</div>
+        }
     }
 }
 
