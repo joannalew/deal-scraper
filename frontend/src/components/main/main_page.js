eb4fdeecad96c7e10ced754e29197ead7cce28bb
Dennis <dmyasnyankin@berkeley.edu>
2019-01-06 18:53:19 -0800

    Add styling to splash page, add resizing to navbar

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './main_page.css';
 
 class MainPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1></h1>
+            <div className="main_page">
+                <h1 className="main_title">Deal Cliq</h1>
                 <footer>
                     
                 </footer>
