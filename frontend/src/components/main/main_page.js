9c74fbeb91c08e3f3383839c3bd3f81cacf0f9ca
Dennis <dmyasnyankin@berkeley.edu>
2019-01-07 09:38:23 -0800

    Create root background image in CSS to persist throughout site

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -6,8 +6,9 @@ class MainPage extends React.Component {
     render() {
         return (
             <div className="main_page">
+                <div className="main_block"></div>
                 <h1 className="main_title">Deal Cliq</h1>
-                <footer>
+                <footer className="main_footer">
                     
                 </footer>
             </div>
