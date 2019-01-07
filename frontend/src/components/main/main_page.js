9ed1a9ed16518bfa1e18e6b4af6dbfa75ccd9eaf
Dennis <dmyasnyankin@berkeley.edu>
2019-01-07 10:27:06 -0800

    Style main page photo and navbar border outline

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -6,9 +6,15 @@ class MainPage extends React.Component {
     render() {
         return (
             <div className="main_page">
-                <div className="main_block"></div>
-                <h1 className="main_title">Deal Cliq</h1>
-                <footer className="main_footer">
+                <div className="main_block_left"></div>
+                <div className="main_words">
+                    <h1 className="main_title">Deal Cliq</h1>
+                    <h2 className="main_description">The Best Deals at the Tip of Your Fingers</h2>
+                </div>
+                <div className="main_image"></div>
+                <div className="main_block_right"></div>
+
+                <footer>
                     
                 </footer>
             </div>
