9363f6f6d9aea0e6fb1d4f71ada232ab66f98450
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-08 11:07:21 -0800

    Add skeleton for recently viewed container

---
+++ frontend/src/components/recently_viewed/recently_viewed_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import RecentlyViewedItems from './recently_viewed';
+
+const mapStateToProps = state => {
+
+    return ({
+
+    });
+};
+
+const MapDispatchToProps = dispatch => {
+
+    return ({
+
+    });
+};
+
+export default connect(mapStateToProps, MapDispatchToProps)(RecentlyViewedItems);
