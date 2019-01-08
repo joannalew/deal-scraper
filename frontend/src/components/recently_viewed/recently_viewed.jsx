41ff84b23efe10aa793d46cd25d94f498b390211
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-08 11:30:26 -0800

    Fill in component

--- frontend/src/components/recently_viewed/recently_viewed.jsx
+++ frontend/src/components/recently_viewed/recently_viewed.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class RecentlyViewedItems extends React.Component {
 
+    componentDidMount() {
+        this.props.recentlyViewed();
+    }
+
     render () {
 
+        const items = this.props.items.map((item, idx) => {
+            
+            return (
+                <Link key={idx} to={`item/${item._id}`}>
+                    <img src={item.storeImg} alt=""/>
+                    <div>{item.title}</div>
+                    <div>{item.price}</div>
+                </Link>
+            )
+        });
+
+        return (
+            <ul>
+                {items}
+            </ul>
+        )
     }
 }
 
