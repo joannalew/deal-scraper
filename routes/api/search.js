3d50abe4e33278b3b0bff90dc338e2ced2a61a19
Joanna <lewj@oregonstate.edu>
2019-01-08 14:13:09 -0800

    add gzip for amazon search

--- routes/api/search.js
+++ routes/api/search.js
@@ -2,12 +2,14 @@ const express = require("express");
 const request = require('request');
 const Promise = require('promise');
 const cheerio = require('cheerio');
+const http = require('https');
+const zlib = require("zlib");
 const router = express.Router();
 
 const ebayKey = require('../../config/keys').ebayAPIKey;
 const etsyKey = require('../../config/keys').etsyAPIKey;
 
-const getImageFromEbay = async function(itemObj, res) {
+const getImageFromEbay = async function (itemObj, res) {
     var promiseArray = [];
 
     for (let key in itemObj) {
@@ -19,7 +21,7 @@ const getImageFromEbay = async function(itemObj, res) {
         url += "&version=967";
         url += `&ItemID=${itemObj[key].storeId}`;
 
-        promiseArray.push(new Promise((resolve, reject) => 
+        promiseArray.push(new Promise((resolve, reject) =>
             request(url, function (error, response, html) {
                 if (error) { reject(error); }
                 let resStr = JSON.stringify(response);
@@ -36,7 +38,7 @@ const getImageFromEbay = async function(itemObj, res) {
     res.send({ items: await Promise.all(promiseArray) });
 };
 
-const getImageFromEtsy = async function(itemObj, res) {
+const getImageFromEtsy = async function (itemObj, res) {
     var promiseArr = [];
 
     for (let key in itemObj) {
@@ -46,7 +48,7 @@ const getImageFromEtsy = async function(itemObj, res) {
 
         await new Promise((res) => setTimeout(res, 100));
 
-        promiseArr.push(new Promise((resolve, reject) => 
+        promiseArr.push(new Promise((resolve, reject) =>
             request(url, function (error, response, html) {
                 if (error) { reject(error); }
                 let resStr = JSON.stringify(response);
@@ -63,7 +65,7 @@ const getImageFromEtsy = async function(itemObj, res) {
     res.send({ items: await Promise.all(promiseArr) });
 }
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
     let id = req.params.id;
 
     let url = "http://open.api.ebay.com/shopping";
@@ -86,18 +88,79 @@ router.get('/:id', function(req, res) {
     })
 });
 
-router.get('/etsy/:keywords', function(req, res) {
+router.get('/amazon/:keywords', function (req, res) {
+    let keywords = req.params.keywords;
+    var urlAmazon = `https://www.amazon.com/s/field-keywords=${keywords}`;
+
+    request(urlAmazon, function (error, response, html) {
+        if (!error) {
+
+            var $ = cheerio.load(html);
+            var itemObj = { 1: {}, 2: {}, 3: {}, 4: {}, 5: {} };
+            var children = {};
+
+            $('#s-results-list-atf').filter(function () {
+                var data = $(this);
+                children.title = data.children().first().attr('id');
+            })
+
+        }
+        console.log($('#atfResults').attr('id'));
+        res.send($('#atfResults').attr('id'));
+    })
+});
+
+router.get('/cheerio/test', function (req, res) {
+    var url = `https://www.amazon.com/s/field-keywords=sweaters`;
+
+    function getGzipped(url, callback) {
+        // buffer to store the streamed decompression
+        var buffer = [];
+
+        http.get(url, function (res) {
+            // pipe the response into the gunzip to decompress
+            var gunzip = zlib.createGunzip();
+            res.pipe(gunzip);
+
+            gunzip.on('data', function (data) {
+                // decompression chunk ready, add it to the buffer
+                buffer.push(data.toString())
+            }).on("end", function () {
+                // response and decompression complete, join the buffer and return
+                callback(null, buffer.join(""));
+            }).on("error", function (e) {
+                callback(e);
+            })
+        }).on('error', function (e) {
+            callback(e)
+        });
+    }
+
+    getGzipped(url, function (err, data) {
+        var $ = cheerio.load(data);
+        var children = {};
+
+        $('#s-results-list-atf').filter(function () {
+            var data = $(this);
+            children.title = data.children().first().attr('id');
+        })
+
+        res.send(children);
+    });
+})
+
+router.get('/etsy/:keywords', function (req, res) {
     var urlEtsy = 'https://openapi.etsy.com/v2/listings/active';
     urlEtsy += `?keywords=${req.params.keywords}`;
     urlEtsy += `&api_key=${etsyKey}`;
 
-    request(urlEtsy, function(error, response) {
+    request(urlEtsy, function (error, response) {
         if (!error) {
             let resStr = JSON.stringify(response);
             let resObj = JSON.parse(resStr);
             let resBody = resObj.body;
             let resReal = JSON.parse(resBody)
-            
+
             let items = resReal.results;
             let count = 25;
             let itemInfo = {};
@@ -105,7 +168,7 @@ router.get('/etsy/:keywords', function(req, res) {
             for (let i = 0; i < count; i++) {
                 itemInfo[i] = {
                     store: 'etsy',
-                    storeId: items[i].listing_id, 
+                    storeId: items[i].listing_id,
                     storeUrl: items[i].url.slice(0, -52),
                     title: items[i].title,
                     price: items[i].price
@@ -117,7 +180,7 @@ router.get('/etsy/:keywords', function(req, res) {
     })
 });
 
-router.get('/ebay/:keywords', function(req, res) {
+router.get('/ebay/:keywords', function (req, res) {
     let callback = '_cb_findItemsByKeywords';
     let keywords = req.params.keywords;
 
@@ -131,7 +194,7 @@ router.get('/ebay/:keywords', function(req, res) {
     url += "&REST-PAYLOAD";
     url += `&keywords=${keywords}`;
 
-    request(url, function(error, response) {
+    request(url, function (error, response) {
         if (!error) {
             let resStr = JSON.stringify(response);
             let resObj = JSON.parse(resStr);
@@ -150,11 +213,11 @@ router.get('/ebay/:keywords', function(req, res) {
 
                 itemInfo[idArray.length] =
                     {
-                      store: 'ebay',
-                      storeId: items[i].itemId[0], 
-                      storeUrl: items[i].viewItemURL[0],
-                      title: items[i].title[0],
-                      price: items[i].sellingStatus[0].convertedCurrentPrice[0].__value__ + ' ' + items[i].sellingStatus[0].currentPrice[0]['@currencyId']
+                        store: 'ebay',
+                        storeId: items[i].itemId[0],
+                        storeUrl: items[i].viewItemURL[0],
+                        title: items[i].title[0],
+                        price: items[i].sellingStatus[0].convertedCurrentPrice[0].__value__ + ' ' + items[i].sellingStatus[0].currentPrice[0]['@currencyId']
                     };
             }
 
