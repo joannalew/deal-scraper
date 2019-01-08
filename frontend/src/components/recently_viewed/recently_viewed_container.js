37973e512fe0c247ec4081376bd975d384b3e7c8
Jerrik Shaw <jlshaw117@gmail.com>
2019-01-08 11:14:57 -0800

    Fill in container

--- frontend/src/components/recently_viewed/recently_viewed_container.js
+++ frontend/src/components/recently_viewed/recently_viewed_container.js
@@ -1,17 +1,18 @@
 import { connect } from 'react-redux';
 import RecentlyViewedItems from './recently_viewed';
+import { recentlyViewed} from '../../util/item_util';
 
 const mapStateToProps = state => {
 
     return ({
-
+        items: Object.values(state.entities.items)
     });
 };
 
 const MapDispatchToProps = dispatch => {
 
     return ({
-
+        recentlyViewed: () => dispatch(recentlyViewed())
     });
 };
 
