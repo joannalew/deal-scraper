091a60271b09f7335474523ac787f7e708777465
Joanna <lewj@oregonstate.edu>
2019-01-08 17:50:59 -0800

    add keys for heroku

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    ebayAPIKey: process.env.EBAY_API_KEY,
+    etsyAPIKey: process.env.ETSY_API_KEY,
+    secretOrKey: process.env.SECRET_OR_KEY
+}
