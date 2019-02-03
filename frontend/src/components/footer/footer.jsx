496c9ae2e174a90483dfbf42d0c1462647cef5bb
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-03 12:48:00 -0800

    Add footer component

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,26 @@
+function footer() {
+    return (
+        <div className='footer'>
+            <div className='footer-title'>Meet the Developers</div>
+            <div className='developer-info'>
+                <div className='jerrik' >
+                    <div className='person-name'>Jerrik Shaw</div>
+                    <div className='person-links'></div>
+                </div>
+                <div className='joanna' >
+                    <div className='person-name'>Joanna Lew</div>
+                    <div className='person-links'></div>
+                </div>
+                <div className='dennis' >
+                    <div className='person-name'>Dennis Myasnyankin</div>
+                    <div className='person-links'></div>
+                </div>
+                <div className='daniel' >
+                    <div className='person-name'>Daniel Moon</div>
+                    <div className='person-links'></div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
