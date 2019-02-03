141c412b39a3caa0357d49f4014ad01d476b6ea1
Joanna <lewj@oregonstate.edu>
2019-02-02 22:42:26 -0800

    style search bar and marquee

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -5,10 +5,10 @@ import './search.css';
 class Search extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             query: '',
             store: 'ebay'
-         };
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -26,29 +26,38 @@ class Search extends React.Component {
             <div>
                 <div>
                     <marquee className="search-options">Yeezy, iPhone, SmartWater, Supreme, Mug, Coffee, Jordan, Nike, Charger, Molton Brown, Louboutin, App Academy Shirts, Subway, Beats, Northface, Valentino, Protein Bars, Gucci</marquee>
-                    <form className="search-form" onSubmit={ this.handleSubmit }>
+                    <form className="search-form" onSubmit={this.handleSubmit}>
                         <span className="search-text">Find Your Deal Now!</span>
                         <label>
                             <span>
                                 <input className="search-input-bar" type="text" value={this.state.query} onChange={this.update('query')} />
                             </span>
                         </label>
-                        <div className='radio-buttons' >
+                        <div className='radio-buttons'>
                             <div>
-                                <input type='radio' name='store' value='ebay' onChange={this.update('store')}/>
-                                <span>Ebay</span>
+                                <label className="radio-container">
+                                    <input type='radio' name='store' value='ebay' onChange={this.update('store')} />
+                                    <span class="circle"></span>
+                                    <p>Ebay</p>
+                                </label>
                             </div>
                             <div>
-                                <input type='radio' name='store' value='etsy' onChange={this.update('store')}/>
-                                <span>Etsy</span>
+                                <label className="radio-container">
+                                    <input type='radio' name='store' value='etsy' onChange={this.update('store')} />
+                                    <span class="circle"></span>
+                                    <p>Etsy</p>
+                                </label>
                             </div>
                             <div>
-                                <input type='radio' name='store' value='amazon' onChange={this.update('store')}/>
-                                <span>Amazon</span>
+                                <label className="radio-container">
+                                    <input type='radio' name='store' value='amazon' onChange={this.update('store')} />
+                                    <span class="circle"></span>
+                                    <p>Amazon</p>
+                                </label>
                             </div>
                         </div>
                         <div>
-                            <button type="submit">Submit</button>
+                            <button className="search-submit" type="submit">Submit</button>
                         </div>
                     </form>
                 </div>
