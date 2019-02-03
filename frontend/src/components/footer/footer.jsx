7f3046d9de78365eb90b502eb68abaaf48e31310
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-03 12:54:00 -0800

    Add info to footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -5,19 +5,31 @@ function footer() {
             <div className='developer-info'>
                 <div className='jerrik' >
                     <div className='person-name'>Jerrik Shaw</div>
-                    <div className='person-links'></div>
+                    <div className='person-links'>
+                        <a className='footer-portfolio' href="https://www.jerrikshaw.com/" target='_blank'><i class="fas fa-briefcase"></i></a>
+                        <a className='footer-github' href="https://github.com/jlshaw117" target='_blank' ><i class="fab fa-github"></i></a>
+                    </div>
                 </div>
                 <div className='joanna' >
                     <div className='person-name'>Joanna Lew</div>
-                    <div className='person-links'></div>
+                    <div className='person-links'>
+                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
+                        <a className='footer-github' href="https://github.com/joannalew" target='_blank' ><i class="fab fa-github"></i></a>
+                    </div>
                 </div>
                 <div className='dennis' >
                     <div className='person-name'>Dennis Myasnyankin</div>
-                    <div className='person-links'></div>
+                    <div className='person-links'>
+                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
+                        <a className='footer-github' href="https://github.com/dmyasnyankin" target='_blank' ><i class="fab fa-github"></i></a>
+                    </div>
                 </div>
                 <div className='daniel' >
                     <div className='person-name'>Daniel Moon</div>
-                    <div className='person-links'></div>
+                    <div className='person-links'>
+                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
+                        <a className='footer-github' href="https://github.com/moona3k" target='_blank' ><i class="fab fa-github"></i></a>
+                    </div>
                 </div>
                 
             </div>
