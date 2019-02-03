ac5acc6e9e64691c0385b6cad687be785895dfe9
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-03 13:24:09 -0800

    Change class names to keep code dry

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
+import './footer.css';
 
 function Footer() {
     return (
-        <div className='footer'>
-            <div className='footer-title'>Meet the Developers</div>
-            <div className='developer-info'>
-                <div className='jerrik' >
-                    <div className='person-name'>Jerrik Shaw</div>
-                    <div className='person-links'>
-                        <a className='footer-portfolio' href="https://www.jerrikshaw.com/" target='_blank'><i class="fas fa-briefcase"></i></a>
-                        <a className='footer-github' href="https://github.com/jlshaw117" target='_blank' ><i class="fab fa-github"></i></a>
+        <footer>
+            <div className='footer-outer'>
+                <div className='footer-inner'>
+                    <div className='footer-title'>Meet the Developers</div>
+                    <div className='developer-info'>
+                        <div className='developer' >
+                            <div className='person-name'>Jerrik Shaw</div>
+                            <div className='person-links'>
+                                <a className='footer-portfolio' href="https://www.jerrikshaw.com/" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-github' href="https://github.com/jlshaw117" target='_blank' ><i className="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        <div className='developer' >
+                            <div className='person-name'>Joanna Lew</div>
+                            <div className='person-links'>
+                                <a className='footer-portfolio' href="" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-github' href="https://github.com/joannalew" target='_blank' ><i className="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        <div className='developer' >
+                            <div className='person-name'>Dennis Myasnyankin</div>
+                            <div className='person-links'>
+                                <a className='footer-portfolio' href="" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-github' href="https://github.com/dmyasnyankin" target='_blank' ><i className="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        <div className='developer' >
+                            <div className='person-name'>Daniel Moon</div>
+                            <div className='person-links'>
+                                <a className='footer-portfolio' href="" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-github' href="https://github.com/moona3k" target='_blank' ><i className="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
-                <div className='joanna' >
-                    <div className='person-name'>Joanna Lew</div>
-                    <div className='person-links'>
-                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
-                        <a className='footer-github' href="https://github.com/joannalew" target='_blank' ><i class="fab fa-github"></i></a>
-                    </div>
-                </div>
-                <div className='dennis' >
-                    <div className='person-name'>Dennis Myasnyankin</div>
-                    <div className='person-links'>
-                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
-                        <a className='footer-github' href="https://github.com/dmyasnyankin" target='_blank' ><i class="fab fa-github"></i></a>
-                    </div>
-                </div>
-                <div className='daniel' >
-                    <div className='person-name'>Daniel Moon</div>
-                    <div className='person-links'>
-                        <a className='footer-portfolio' href="" target='_blank'><i class="fas fa-briefcase"></i></a>
-                        <a className='footer-github' href="https://github.com/moona3k" target='_blank' ><i class="fab fa-github"></i></a>
-                    </div>
-                </div>
-                
             </div>
-        </div>
+        </footer>
     )
 }
 
