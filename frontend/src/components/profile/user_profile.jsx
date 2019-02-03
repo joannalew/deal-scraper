cc5c8c317e8e17a4ae6945b02b77dfcf6415f2b1
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-03 11:38:43 -0800

    Add image to page

--- frontend/src/components/profile/user_profile.jsx
+++ frontend/src/components/profile/user_profile.jsx
@@ -27,7 +27,7 @@ class UserProfile extends React.Component {
                 <div className='spacer'></div>
                 <div className='user-profile-info'>
                     <div >
-                        <img className='user-profile-img' src="https://profiles.utdallas.edu/img/default.png" alt=""/>
+                        <div className='user-profile-img'></div>
                     </div>
                     <div className='user-profile-details'>
                         <div className='user-profile-username'>{this.props.user.username}</div>
