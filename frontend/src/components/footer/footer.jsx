dce557409bc94cfe6a5c60544d22ded915c7caa2
Jerrik Shaw <jlshaw117@gmail.com>
2019-02-04 09:59:20 -0800

    Add links to sites

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -18,14 +18,14 @@ function Footer() {
                         <div className='developer' >
                             <div className='person-name'>Joanna Lew</div>
                             <div className='person-links'>
-                                <a className='footer-portfolio' href="" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-portfolio' href="https://www.joannalew.com/" target='_blank'><i className="fas fa-briefcase"></i></a>
                                 <a className='footer-github' href="https://github.com/joannalew" target='_blank' ><i className="fab fa-github"></i></a>
                             </div>
                         </div>
                         <div className='developer' >
                             <div className='person-name'>Dennis Myasnyankin</div>
                             <div className='person-links'>
-                                <a className='footer-portfolio' href="" target='_blank'><i className="fas fa-briefcase"></i></a>
+                                <a className='footer-portfolio' href="https://www.dennismyasnyankin.com/" target='_blank'><i className="fas fa-briefcase"></i></a>
                                 <a className='footer-github' href="https://github.com/dmyasnyankin" target='_blank' ><i className="fab fa-github"></i></a>
                             </div>
                         </div>
